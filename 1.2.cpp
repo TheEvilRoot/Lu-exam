06bf056f3b369656214d901ea346fdf82d5fa397
ynres <artemslavuk@gmail.com>
2018-11-03 16:22:42 +0300

    *

--- 1.2.cpp
+++ 1.2.cpp
@@ -4,18 +4,28 @@
 #include "stdlib.h"
 #include "cmath"
 int lengthOfString(char*);
-void transformOfString(int*,char*,int);
+void transformOfString(int*, char*, int);
 
 int main(){
 	int size = 1000;
-	char* string=(char*)malloc(size);
+	char* string = (char*)malloc(size);
 	gets(string);
 	fflush(stdin);
 	int length = lengthOfString(string);
-	int*array = (int*)malloc(sizeof(int)*length);
-	transformOfString(array,string, length);
+	int*array = (int*)calloc(sizeof(int),length);
+	int counter = 0, numberOfElement = 0;
 	for (int i = 0; i < length; i++){
-		printf("%5d", array[i]);
+		if ((string[i] >= '0') && (string[i] <= '9')){
+			array[numberOfElement] += (string[i] - '0')*pow(10, counter);
+			counter++;
+		}
+		else{
+			numberOfElement++;
+			counter = 0;
+		}
+	}
+	for (int i = 0; i < length; i++){
+		printf("%3d", *(array + i));
 	}
 	puts("");
 	return 0;
@@ -26,17 +36,4 @@ int lengthOfString(char*string){
 		i++;
 	}
 	return i;
-}
-void transformOfString(int*array,char*string, int length){
-	int counter = 0, numberOfElement=0;
-	for (int i = 0; i < length; i++){
-		if ((string[i] >= '0') && (string[i] <= '9')){
-			array[numberOfElement] += (string[i] - '0')*pow(10, counter);
-			counter++;
-		}
-		else if ((string[i] == ' ') || (string[i] == '\0')){
-			counter = 0;
-			numberOfElement++;
-		}
-	}
 }
